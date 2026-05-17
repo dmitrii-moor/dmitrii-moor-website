1f9d1731cf5d85a0dbc079414425e289d83251d3
dmitrii-moor <dmitry.moor@gmail.com>
2026-05-17 23:54:14 +0100

    upd

--- personal-site/app/layout.tsx
+++ personal-site/app/layout.tsx
@@ -16,6 +16,11 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Dmitrii Moor",
   description: "Personal research website",
+  icons: {
+    icon: "/icon.png",
+    shortcut: "/icon.png",
+    apple: "/icon.png",
+  },
 };
 
 export default function RootLayout({
